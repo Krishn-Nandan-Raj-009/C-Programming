3e854ef27e41e102d969b1ee122b73f2a89f19f1
Krishn_Nandan_Raj__009 <krishnanandanraj007@gmail.com>
2026-06-01 22:18:32 +0530

    Add programs for prime numbers, Armstrong number, and Fibonacci

--- V-V-I problem.c
+++ V-V-I problem.c
@@ -57,4 +57,76 @@ int main() {
     return 0;
 }
 
+🔹 1. Print all Prime Numbers from 1 to N
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i++) {
+        isPrime = 1;
+
+        for(j = 2; j <= i/2; j++) {
+            if(i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if(isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
+🔹 2. Check Armstrong Number
+#include <stdio.h>
+
+int main() {
+    int num, original, remainder, result = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while(num != 0) {
+        remainder = num % 10;
+        result += remainder * remainder * remainder;
+        num /= 10;
+    }
+
+    if(result == original)
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong Number");
+
+    return 0;
+}
+🔹 3. Fibonacci Series up to N Terms
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a = 0, b = 1, next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for(i = 1; i <= n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
+
 
