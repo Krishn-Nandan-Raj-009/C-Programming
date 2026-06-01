495fe4b95543deaf2392348e320f919f32b12a9c
Krishn_Nandan_Raj__009 <krishnanandanraj007@gmail.com>
2026-06-01 19:27:50 +0530

    Add files via upload
    Problems related to function in C language all topic covered

---
+++ Fbasic.c
@@ -0,0 +1,277 @@
+#include<stdio.h>
+#include<math.h>
+
+
+// void sum();              // declare the function    or prototype   or signature       
+// int main (){
+//     sum();               // calling the function 
+//     return 0;
+    
+// }
+// void sum (){             // defining  the function 
+//     int a,b;
+//     scanf("%d%d",&a,&b);
+//     printf("the summ is %d",a+b);
+// }
+
+// int main(){
+// int a,b;
+// scanf("%d%d",&a,&b);
+// printf("The sum is %d",sum(a,b));
+// return 0;
+
+
+// }
+// int sum (int x,int y)
+
+// {
+//     // printf("%d",a+b);
+//     return a+b;
+// }
+
+// int sum(int,int);
+// int min(int,int);
+// int mul(int,int);
+// int div(int,int);
+
+// int main () {
+//     int a,b;
+//     scanf("%d%d",&a,&b);
+//     printf("%d\n",sum(a,b));
+//     printf("%d\n",min(a,b));
+//     printf("%d\n",mul(a,b));
+//     printf("%d\n",div(a,b));
+//      return 0;
+// }
+
+// int sum(int x,int y) {
+//     return x+y;
+// }
+// int mul(int x,int y) {
+//     return x*y;
+// }
+// int min(int x,int y) {
+//     return x-y;
+// }
+// int div(int x,int y) {
+//     return x/y;
+// }
+
+// void swap(int *,int*);
+// int main() {
+// int a,b;
+// printf(" Enter two number\n");
+// scanf("%d%d",&a,&b);
+// printf("Values before calling: %d %d\n",a,b);
+// swap(&a,&b);
+// printf("values after calling: %d %d\n",a,b);
+
+// }
+
+// void swap(int *x,int *y){
+//     int temp;
+//     temp=*x;
+//     *x=*y;
+//     *y=temp;
+//     printf("values inside function : %d %d\n",*x,*y);
+// }
+
+
+//               RECURSION    
+
+// long long int factorial(int);
+// int main(){
+//     int n;
+//     printf("Enter a positive number");
+//     scanf("%d",&n);
+//     printf("Factorial of %d =%lld",n,factorial(n));
+//     return 0;
+// }
+// long long int factorial(int n){
+//     if (n==0 ||n==1)
+//     return 1;
+//     else {
+//         return (n* factorial(n-1));
+//     }
+// }
+
+
+// long long int natural(int);
+// int main(){
+//     int x;
+//       printf("Enter a number");
+//     scanf("%d",&x);
+  
+//     printf("sum of natural number of %d is %lld",x,natural(x));
+//     return 0;
+// }
+// long long int natural(int n) {
+//     if(n==0)
+//         return 0;
+
+  
+//     else
+//        return n+natural(n-1);
+    
+// }
+
+
+// long long int fibbonacci(int);
+// int main(){
+//     int x;
+//     printf("Enter a positive number");
+//     scanf("%d",&x);
+//     printf("The fibonnacci of %d is %lld\n and all indivisual is ",x,fibbonacci(x));
+//     for (int i = 0; i<= x; i++)
+//     {
+//         /* code */printf("%lld\t",fibbonacci(i));
+//     }
+    
+//     return 0;
+// }
+
+// long long int fibbonacci(int n){
+//     if (n==0 ||n==1) return n;
+//     else return fibbonacci(n-1)+fibbonacci(n-2);
+
+    
+// }
+
+// int main(){
+//     double x=9.0, y=8.0,z=7.0;
+//     printf("\nLog value is :%lf",log(x));
+//      printf("\nLog value with base 10 is :%lf",log10(x));
+//       printf("\n exponential value is :%lf",exp(x));
+//       printf("\n ceil value is :%lf",ceil(8.94));
+//        printf("\n floor value is :%lf",floor(2.34));
+//         printf("\nPower value is :%lf",pow(3.0,2.0));
+//          printf("\n square root value is :%lf",sqrt(16.0));
+//           printf("\n absolute value is :%lf",fabs(-5.0));
+//            printf("\n remainder value is :%lf",fmod(10.0,3.0));
+//             printf("\n sine value is :%lf",sin(90));
+//              printf("\n cosine value is :%lf",cos(90));
+//               printf("\n tangent value is :%lf",tan(90));
+//                printf("\n arc tangent value is :%lf",atan2(y,z));
+//         return 0;
+// }
+
+// int main() {
+
+// int a=5; {
+
+//      a=50;{
+//          int a=500;
+//             printf("%d\n",a);
+//         }
+//         printf("%d\n",a);
+//     }
+//     printf("%d\n",a);
+
+
+
+
+
+// void fun1() {
+//     static int x;
+//     x=10;
+//     x++;
+//     printf("%d\t",x);
+// }
+
+// int main ()  {
+//     fun1();
+//     fun1();
+//     fun1();
+// return 0;
+// }
+
+// float cgpa=100  ;   // global variable
+// # include"switch.c"
+// int main() {
+    
+//     extern float cgpa;
+//     // cgpa=6;
+//     printf("%f",cgpa);
+
+   
+
+// int balance = 1000;  // global variable
+
+
+// void checkbalance() {
+//     printf("Your balance is %d\n", balance);
+// }
+
+// void deposit() {
+//     int amount;
+//     printf("Enter amount to deposit");
+//     scanf("%d",&amount);
+//     balance += amount;
+//     printf("Deposited %d New balance is %d\n", amount, balance);
+// }
+
+
+
+// int main() {
+// int choice;
+// char again;
+// do{
+//     printf("\n --- Banking Menu----\n");
+//     printf("1. Checking\n");
+//      printf("2. Deposit money\n");
+//       printf("Enter your choice\n");
+//       scanf("%d",&choice);
+
+
+//       switch (choice)
+//       {
+//       case 1:
+//         /* code */ checkbalance();
+//         break;
+      
+//         case 2:
+//         /* code */ deposit();
+//         break;
+//       default:
+//       printf("Invalid choice")
+//       ;
+//       }
+//       printf("\n do you want to perform another operation ?(y/n)" );
+//       scanf(" %c",&again);
+// }
+// while (again== 'y' || again== 'Y');
+// {
+//     /* code */  printf("Thank you for using our banking system. Goodbye!\n");
+// }
+
+
+// return 0;
+
+// }
+
+
+
+
+int c(int x,int y) {
+  int t=y;
+  x=y;
+  y=t;
+  printf("Inside function: a=%d, b=%d\n",x,y);
+}
+
+int main() {
+    int a,b;
+    printf("Enter two numbers");
+    scanf("%d %d",&a,&b);
+    printf("Before swapping, the numbers are a=%d, b=%d\n",a,b);
+    c(a,b);
+    printf("After swapping, the numbers are a=%d, b=%d",a,b);
+   
+}
+
+
+
+
+
+
+
