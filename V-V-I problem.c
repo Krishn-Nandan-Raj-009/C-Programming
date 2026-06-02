5acea80f2a38d38e8e871f2ce610b962b65d71de
Krishn_Nandan_Raj__009 <krishnanandanraj007@gmail.com>
2026-06-02 11:30:13 +0530

    Add menu-driven program for various operations

--- V-V-I problem.c
+++ V-V-I problem.c
@@ -130,3 +130,77 @@ int main() {
 }
 
 
+
+5. Menu-Driven Program (Loop + Switch)
+#include <stdio.h>
+
+int main() {
+    int choice, num, i, fact;
+    
+    while(1) {
+        printf("\nMenu:\n");
+        printf("1. Even/Odd\n");
+        printf("2. Prime Check\n");
+        printf("3. Factorial\n");
+        printf("4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+
+        case 1:
+            printf("Enter number: ");
+            scanf("%d", &num);
+            if(num % 2 == 0)
+                printf("Even\n");
+            else
+                printf("Odd\n");
+            break;
+
+        case 2:
+            printf("Enter number: ");
+            scanf("%d", &num);
+
+            int isPrime = 1;
+            for(i = 2; i <= num/2; i++) {
+                if(num % i == 0) {
+                    isPrime = 0;
+                    break;
+                }
+            }
+
+            if(num <= 1)
+                isPrime = 0;
+
+            if(isPrime)
+                printf("Prime\n");
+            else
+                printf("Not Prime\n");
+            break;
+
+        case 3:
+            printf("Enter number: ");
+            scanf("%d", &num);
+
+            fact = 1;
+            for(i = 1; i <= num; i++) {
+                fact *= i;
+            }
+
+            printf("Factorial = %d\n", fact);
+            break;
+
+        case 4:
+            printf("Exiting...\n");
+            return 0;
+
+        default:
+            printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}  
+
+
+
+
