6d63faa89dce6d553bf6733a1087864e61774bb9
Krishn_Nandan_Raj__009 <krishnanandanraj007@gmail.com>
2026-06-04 22:55:55 +0530

    Update basic.c

--- basic.c
+++ basic.c
@@ -1,62 +1,62 @@
 #include "stdio.h"
   int main(){
     
-    // char was = "w";
-  //  printf("first c program\n");
-  //  printf("workingtwith\tbasic\n"); 
-  //  a=10,b=20;
-  //  printf("%d\n %d\n  %d\n",a,b,c=a+b);
-  //  printf("the sum is %d\n",c+10);
-  //  printf("the sum of %d and %d is %d",a,b,c);
+    char was = "w";
+   printf("first c program\n");
+   printf("workingtwith\tbasic\n"); 
+   a=10,b=20;
+   printf("%d\n %d\n  %d\n",a,b,c=a+b);
+   printf("the sum is %d\n",c+10);
+   printf("the sum of %d and %d is %d",a,b,c);
 
-  // printf("%c",was);
-  //  printf("%d",ch)   
+  printf("%c",was);
+   printf("%d",ch)   
 
   
-  // use of data type   range  and bits 
+  use of data type   range  and bits 
 
 
-// char ch =123;
-// printf("%c\n",ch);
-// int a=85;
-// printf("%d\n",a);
-// long int b=34343477;
-// printf("%ld\n",b);
-// short int c=3434;
-// printf("%hd\n",c);
-//  long int v=3437986843;
-// printf("%ld\n",v);
-// long long int d=3437986843466876;
-// printf("%lld\n",d);
-// float e=3.03;
-// printf("%f\n",e);
-// double f=3.037575;
-// printf("%lf\n",f);
-// long double g=3.0387585878;
-// printf("%Lf\n",g);
-// 
+char ch =123;
+printf("%c\n",ch);
+int a=85;
+printf("%d\n",a);
+long int b=34343477;
+printf("%ld\n",b);
+short int c=3434;
+printf("%hd\n",c);
+ long int v=3437986843;
+printf("%ld\n",v);
+long long int d=3437986843466876;
+printf("%lld\n",d);
+float e=3.03;
+printf("%f\n",e);
+double f=3.037575;
+printf("%lf\n",f);
+long double g=3.0387585878;
+printf("%Lf\n",g);
 
 
-//  use of scanf and printf
 
-// int k,i;
-// int m;
-// scanf("%d%d",&k,&i);
-//  m=k+i;
-// printf("the sum of %d and %d is %d",k,i,m);
+ use of scanf and printf
 
-// int x,z,r;
-// scanf("%d%d%d",&x,&z,&r);
-// float y=(x+z+r)/3;
-// printf("%f",y);
+int k,i;
+int m;
+scanf("%d%d",&k,&i);
+ m=k+i;
+printf("the sum of %d and %d is %d",k,i,m);
 
-// int s,ta,da,t,td;
-// scanf("%d%d%d",&s,&ta,&da) ;
-// // ta =0.05;
-// // da=0.02;
-// td=ta+da;
-// t=s+(s*(td)); 
-// printf("%d",t);
+int x,z,r;
+scanf("%d%d%d",&x,&z,&r);
+float y=(x+z+r)/3;
+printf("%f",y);
+
+int s,ta,da,t,td;
+scanf("%d%d%d",&s,&ta,&da) ;
+ ta =0.05;
+ da=0.02;
+td=ta+da;
+t=s+(s*(td)); 
+printf("%d",t);
 
   return 0;
 }
