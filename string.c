2c539353da4274153d098c0a6e7dd1d797964eca
Krishn_Nandan_Raj__009 <krishnanandanraj007@gmail.com>
2026-06-07 13:36:35 +0530

    Refactor string input and character counting logic
    Reintroduced scanf for user input and added character printing in the loop.

--- string.c
+++ string.c
@@ -2,16 +2,16 @@
 
 int main() {
      char name[20];
-    //  scanf("%s",name);
+     scanf("%s",name);
 
      fgets(name,8,stdin);
      int i=0,count=0;
 
 
      while(name[i]!='\0'){
-        // printf("%c\n",name[i]);
-        // i++;
-        // count++;
+        printf("%c\n",name[i]);
+        i++;
+        count++;
 
         if(name[i]=='a'|| name[i]=='e'||name[i]=='i' ||name[i]=='u'||name[i]=='o' )  {
             count++;
